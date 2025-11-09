e0cba3cccb0b69b382315b352f44dbfc4363dc6b
TysboProg <166642110+TysboProg@users.noreply.github.com>
2025-11-10 00:25:57 +0300

    update test database

--- backend/tests/unit/test_database.py
+++ backend/tests/unit/test_database.py
@@ -5,6 +5,6 @@ from sqlalchemy import text
 @pytest.mark.asyncio
 async def test_database(db_session):
     """Test that the database connection works."""
-    result = await db_session.execute(text("SELECT 1"))
+    result = await db_session.execute(text("SELECT current_database()"))
     value = result.scalar()
-    assert value == 1
+    assert value == "tests"
